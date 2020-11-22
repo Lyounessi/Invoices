6a73fafd971eb9a0f302d67e147b89c83cea8627
Mourad Boukroun <boukroun@gmail.com>
2020-11-22 18:07:23 +0100

    html form create company

--- Invoicing/users/models.py
+++ Invoicing/users/models.py
@@ -4,16 +4,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=50)
 
-    
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
@@ -36,20 +36,23 @@ class Logo(models.Model):
         return reverse("_detail", kwargs={"pk": self.pk})
 """
 
+
 class Company(models.Model):
-    
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    activityDom = models.CharField(max_length=50)#must be converted to a choices field
-    adress = models.CharField(max_length=100)
-    countrie = models.CharField(max_length=50)#must be converted to a choices field
-    city = models.CharField(max_length=50)#must be converted to a choices field(algo of countries)
+    # must be converted to a choices field
+    activityDom = models.CharField(max_length=50)
+    adress = models.TextField()
+    # must be converted to a choices field
+    countrie = models.CharField(max_length=50)
+    # must be converted to a choices field(algo of countries)
+    city = models.CharField(max_length=50)
     postCode = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     taxNum = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50, blank=True)
     logo = models.ImageField(blank=True)
-
 
     def __str__(self):
         return self.name
--- Invoicing/users/views.py
+++ Invoicing/users/views.py
@@ -13,7 +13,6 @@ from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 
 
-
 ################################ user's Views #########################################
 
 
@@ -37,17 +36,17 @@ def create_user(request):
     this view is dedicated to create user account as signup
     """
     if request.method == "POST":
-        
+
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()  # load the profile instance created by the signal
             user.save()
-            
+
             return redirect('users:home')
         else:
             messages.error(request, 'The form is invalid.')
-        
+
     else:
 
         form = SignUpForm()
@@ -55,33 +54,33 @@ def create_user(request):
     return render(request, 'registration/signup.html', {"form": form})
 
 
-
-
 ################################ Company's Views #########################################
 
-@method_decorator(login_required, name='dispatch')
+# @method_decorator(login_required, name='dispatch')
 class CreateCompany(CreateView):
     """
     Create New Caompany for a specific user
     """
     model = Company
     template_name = 'users/companys/company.html'
-    fields = ('name', 'activityDom', 'logo', 'adress', 'countrie', 'city', 'postCode', 'email', 'taxNum', 'phone', )
+    fields = ('name', 'activityDom', 'logo', 'adress', 'countrie',
+              'city', 'postCode', 'email', 'taxNum', 'phone', )
     success_url = reverse_lazy('users:home')
-   
-   
+
     def form_valid(self, form):
         """
         Add a valid form Calling the auth user and 
         give it as value to the owner field
         """
-        
+
         obj = form.save(commit=False)
         obj.owner = self.request.user
-        obj.save()        
+        obj.save()
         return super(CreateCompany, self).form_valid(form)
 
-@method_decorator(login_required, name='dispatch')
+# @method_decorator(login_required, name='dispatch')
+
+
 class CompanyDetailView(DetailView):
     """
     showing details of a company
@@ -90,7 +89,9 @@ class CompanyDetailView(DetailView):
     template_name = 'users/companys/details.html'
     context_object_name = 'company'
 
-@method_decorator(login_required, name='dispatch')
+# @method_decorator(login_required, name='dispatch')
+
+
 class CompanyUpdateView(UpdateView):
     """
     Update the company's Informations
@@ -98,14 +99,14 @@ class CompanyUpdateView(UpdateView):
     model = Company
     template_name = 'users/companys/update.html'
     context_object_name = 'company'
-    fields = ('name', 'activityDom', 'logo', 'adress', 'countrie', 'city', 'postCode', 'email', 'taxNum', 'phone', )
-
+    fields = ('name', 'activityDom', 'logo', 'adress', 'countrie',
+              'city', 'postCode', 'email', 'taxNum', 'phone', )
 
     def get_success_url(self):
         return reverse_lazy('users:companyDetail', kwargs={'pk': self.object.id})
 
- 
-@method_decorator(login_required, name='dispatch')
+
+# @method_decorator(login_required, name='dispatch')
 class CompanyDeleteView(DeleteView):
     """
     Delete a company
